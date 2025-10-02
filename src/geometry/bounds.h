050ab3a23369b383c7386ffd70e48e48f61e8242
alinimi <alicia.nicasmiquel@gmail.com>
2025-10-02 18:21:41 +0200

    Add (unused) longest_axis function

--- src/geometry/bounds.h
+++ src/geometry/bounds.h
@@ -5,8 +5,35 @@
 #include "interval.h"
 
 namespace geom {
+    // TODO: Clarify why the separation between Bounds and AABB exists: will we use local coordinates
+    // for something? move AABB functionality to AABB
+    // using AABB = std::array<Interval, 3>;
+    struct AABB {
+        Interval operator [](int dim) const {
+            switch (dim) {
+            case 0:
+                return x;
+            case 1:
+                return y;
+            case 2:
+                return z;
+            default:
+                assert(false && "Accessing invalid dimension of AABB");
+                throw;
+            }
+        }
 
-    using AABB = std::array<Interval, 3>;
+        int longest_axis() {
+            if (x.size() > y.size())
+                return x.size() > z.size() ? 0 : 2;
+            else
+                return y.size() > z.size() ? 1 : 2;
+        }
+
+        Interval x;
+        Interval y;
+        Interval z;
+    };
 
     struct Bounds {
         constexpr Bounds() {}
@@ -18,22 +45,21 @@ namespace geom {
         constexpr Bounds(const Bounds& b1, const Bounds& b2)
         {
             world_bounds = {
-                Interval(b1.world_bounds[0], b2.world_bounds[0]),
-                Interval(b1.world_bounds[1], b2.world_bounds[1]),
-                Interval(b1.world_bounds[2], b2.world_bounds[2])
+                Interval(b1.world_bounds.x, b2.world_bounds.x),
+                Interval(b1.world_bounds.y, b2.world_bounds.y),
+                Interval(b1.world_bounds.z, b2.world_bounds.z)
             };
         }
         constexpr Bounds padded()
         {
             return  {
                 {
-                    Interval(world_bounds[0], world_bounds[0]).expand(1e-5),
-                    Interval(world_bounds[1], world_bounds[1]).expand(1e-5),
-                    Interval(world_bounds[2], world_bounds[2]).expand(1e-5)
+                    Interval(world_bounds.x, world_bounds.x).expand(1e-5),
+                    Interval(world_bounds.y, world_bounds.y).expand(1e-5),
+                    Interval(world_bounds.z, world_bounds.z).expand(1e-5)
                 }
             };
         }
-
         AABB world_bounds;
     };
 
